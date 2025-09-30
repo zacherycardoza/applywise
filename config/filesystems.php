83c25f74e31c210fc563cacaed00cd448cabb548
zacherycardoza <85895258+zacherycardoza@users.noreply.github.com>
2025-09-30 15:36:28 -0400

    provided s3 fallback region

--- config/filesystems.php
+++ config/filesystems.php
@@ -51,7 +51,7 @@ return [
             'driver' => 's3',
             'key' => env('AWS_ACCESS_KEY_ID'),
             'secret' => env('AWS_SECRET_ACCESS_KEY'),
-            'region' => env('AWS_DEFAULT_REGION'),
+            'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
             'bucket' => env('AWS_BUCKET'),
             'endpoint' => env('AWS_ENDPOINT'),
             'use_path_style_endpoint' => true,
